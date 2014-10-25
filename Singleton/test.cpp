6063eace8828a2403eb94ac15d288d9e63f336df
zkw <zhangkewei_2014@163.com>
2014-10-25 15:27:15 +0800

    多线程情况下进行测试，出现问题

--- Singleton/test.cpp
+++ Singleton/test.cpp
@@ -1,19 +1,35 @@
 /**************************************************************
-	> file: main.cpp
+	> file: test.cpp
 	> by: zkw
 	> description:
  **************************************************************/
 #include <iostream>
+#include <vector>
+#include <pthread.h>
 #include "Singleton.h"
 using namespace std;
 
+void *threadFunc(void *arg)
+{
+	Singleton *p = Singleton::getInstance();
+	cout << p << endl;
+
+	return NULL;
+}
+
 int main(void)
 {
-	Singleton *p1 = Singleton::getInstance();
-	Singleton *p2 = Singleton::getInstance();
-	Singleton *p3 = Singleton::getInstance();
+	vector<pthread_t> vec(50);
 
-	cout << p1 << " " << p2 << " " << p3 << endl;
-	
+	vector<pthread_t>::iterator it = vec.begin();
+	for (; it != vec.end(); ++it) {
+		pthread_create(&*it, NULL, threadFunc, NULL);
+	}
+
+	it = vec.begin();
+	for (; it != vec.end(); ++it) {
+		pthread_join(*it, NULL);
+	}	
+
 	return 0;
 }
